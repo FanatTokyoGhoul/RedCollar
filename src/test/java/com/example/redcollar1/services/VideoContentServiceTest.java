fa16980be2b31220c096a41c037d0cfbe167215f
FanatTokyoGhoul <nayzer322@gmail.com>
2021-10-07 22:42:40 +0300

    Update unit test for Services.

--- src/test/java/com/example/redcollar1/services/VideoContentServiceTest.java
+++ src/test/java/com/example/redcollar1/services/VideoContentServiceTest.java
@@ -1,6 +1,7 @@
 package com.example.redcollar1.services;
 
 import com.example.redcollar1.exception.IncorrectNameContentException;
+import com.example.redcollar1.models.dto.request.VideoContentDtoRequest;
 import com.example.redcollar1.models.dto.response.VideoContentDtoResponse;
 import com.example.redcollar1.models.entities.Person;
 import com.example.redcollar1.models.entities.VideoContent;
@@ -19,16 +20,16 @@ import static org.mockito.Mockito.when;
 
 public class VideoContentServiceTest {
 
-
+    private VideoContentDtoRequest modelVideoContentDtoRequestIncorrectName = new VideoContentDtoRequest("rr", "ttt", "image", "it", 1L);
+    private VideoContentDtoRequest modelVideoContentDtoRequest = new VideoContentDtoRequest("rrrr", "ttt", "image", "it", 1L);
     private VideoContentDtoResponse modelVideoContentDto= new VideoContentDtoResponse(1L, "rrrr", "ttt", "image", "it", 1L);
-    private Person modelPerson = new Person(1l, "sss", 18L, LocalDate.of(2001, 3, 9), "zzz", "qqq", "ttt", new ArrayList<>());
-    private VideoContent modelVideoContent = new VideoContent(1L, "rrrr", "ttt", "image", "it", modelPerson);
-    private VideoContent modelVideoContentNullId = new VideoContent(null, "rrrr", "ttt", "image", "it", modelPerson);
+    private Person modelPerson = new Person(1l, "sss", 18L, LocalDate.of(2001, 3, 9), "zzz", "qqq", "ttt", new HashSet<>());
+    private VideoContent modelVideoContent = new VideoContent(1L, "rrrr", "ttt", "image", "it", 1L);
+    private VideoContent modelVideoContentNullId = new VideoContent(null, "rrrr", "ttt", "image", "it", 1L);
 
     private VideoContentRepository repVideoContent = mock(VideoContentRepository.class);
     private PersonRepository repPerson = mock(PersonRepository.class);
-    private CheckDataService dataService = mock(CheckDataService.class);
-    private VideoContentService videoContentService = new VideoContentService(new VideoContentDtoFactory(), repVideoContent, repPerson, dataService);
+    private VideoContentService videoContentService = new VideoContentService(new VideoContentDtoFactory(), repVideoContent, repPerson);
 
     public VideoContentServiceTest() {
         modelPerson.getContents().add(modelVideoContent);
@@ -36,24 +37,22 @@ public class VideoContentServiceTest {
 
     @Test
     public void createVideoContent_incorrectNameContentException(){
-        when(repPerson.getById(1L)).thenReturn(modelPerson);
         assertThrows(IncorrectNameContentException.class, () ->
-                videoContentService.save("as", "testTest", "testTest", "testTest", 1L));
+                videoContentService.save(modelVideoContentDtoRequestIncorrectName));
     }
 
     @Test
     public void createVideoContent_correctData() {
-        when(repPerson.getById(1L)).thenReturn(modelPerson);
         when(repVideoContent.save(modelVideoContentNullId))
                 .thenReturn(modelVideoContent);
-        assertEquals(modelVideoContentDto, videoContentService.save("rrrr", "ttt", "image", "it", 1L));
+        assertEquals(modelVideoContentDto, videoContentService.save(modelVideoContentDtoRequest));
     }
 
     @Test
     public void updateVideoContent_correctData() {
-        when(repPerson.getById(1L)).thenReturn(modelPerson);
+        when(repPerson.findById(1L)).thenReturn(Optional.of(modelPerson));
         when(repVideoContent.save(modelVideoContentNullId))
                 .thenReturn(modelVideoContent);
-        assertEquals(modelVideoContentDto, videoContentService.save("rrrr", "ttt", "image", "it", 1L));
+        assertEquals(modelVideoContentDto, videoContentService.save(modelVideoContentDtoRequest));
     }
 }
